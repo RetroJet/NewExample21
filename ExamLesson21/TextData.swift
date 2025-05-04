b98eee9d8bc73639be3375e9b1cb219995bedc2f
RetroJet <rretrojet@gmail.com>
2025-05-04 23:00:02 +0200

    Added TextData file
    Added TextData file for textlabel

---
+++ ExamLesson21/TextData.swift
@@ -0,0 +1,13 @@
+//
+//  Untitled.swift
+//  ExamLesson21
+//
+//  Created by Nazar on 04.05.2025.
+//
+
+enum TextData {
+    static let mainText = """
+                          Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                          amet, consectetur, adipisci velit...
+                          """
+}
