35345086c38c8624bbc8c3026dceac16a4ec52f1
RetroJet <rretrojet@gmail.com>
2025-05-08 21:55:05 +0200

    Added class CustomButton
    Added class CustomButton and added 3 buttons.

---
+++ ExamLesson21/CustomButton.swift
@@ -0,0 +1,40 @@
+//
+//  CustomButtonView.swift
+//  ExamLesson21
+//
+//  Created by Nazar on 05.05.2025.
+//
+import UIKit
+
+class CustomButton: UIButton {
+    private var shouldAddShadow: Bool
+    
+    init(titleNormal: String, titleColor: UIColor, colorButton: UIColor, shouldAddShadow: Bool = false){
+        self.shouldAddShadow = shouldAddShadow
+        super.init(frame: .zero)
+        setupButton(titleNormal: titleNormal, titleColor: titleColor, colorButton: colorButton)
+        
+    
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButton(titleNormal: String, titleColor: UIColor, colorButton: UIColor) {
+        setTitle(titleNormal, for: .normal)
+        setTitle("", for: .highlighted)
+        setTitleColor(titleColor, for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        backgroundColor = colorButton
+        layer.cornerRadius = 10
+        
+        if shouldAddShadow {
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowOpacity = 0.3
+            layer.shadowOffset = CGSize.init(width: 0, height: 5)
+            layer.shadowRadius = 5
+        }
+    }
+}
