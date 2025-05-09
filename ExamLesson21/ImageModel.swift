a9034f106956cc09afe81acee662b7d2f00fe835
RetroJet <rretrojet@gmail.com>
2025-05-09 14:21:42 +0200

    Added struct ImageModel

---
+++ ExamLesson21/ImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageModel.swift
+//  ExamLesson21
+//
+//  Created by Nazar on 09.05.2025.
+//
+
+import Foundation
+
+struct ImageModel {
+    let imageName: String
+    let text: String
+}
