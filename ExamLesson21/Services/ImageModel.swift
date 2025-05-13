8c70f42d3adabe89e9f71896ae2f616025f28e81
RetroJet <rretrojet@gmail.com>
2025-05-13 20:31:38 +0200

    Added built-in protocols in the project

--- ExamLesson21/Services/ImageModel.swift
+++ ExamLesson21/Services/ImageModel.swift
@@ -5,7 +5,15 @@
 //  Created by Nazar on 09.05.2025.
 //
 
-struct ImageModel {
+struct ImageModel: Comparable, CustomStringConvertible {
     let imageName: String
     let text: String
+    
+    var description: String {
+        "Название картинки: \(imageName), описание: \(text)."
+    }
+    
+    static func < (lhs: ImageModel, rhs: ImageModel) -> Bool {
+        lhs.imageName < rhs.imageName
+    }
 }
